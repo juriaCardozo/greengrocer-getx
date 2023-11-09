f00ce64a7bcec7743bf7a5befbdce5e014820fd6
Júlia Cardozo <ccardozojulia@gmail.com>
2023-11-09 15:01:11 -0300

    Alterando icone e nome do app

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'Greengrocer',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
         scaffoldBackgroundColor: Colors.white.withAlpha(190),
